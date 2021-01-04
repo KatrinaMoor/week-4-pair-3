c16e7fd1f0cd808a9f5bea015f9180657f03bf26
Sherlyn Paterson <sherlynpaterson@gmail.com>
2021-01-04 15:02:57 -0500

    Pair exercise

--- 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
@@ -7,8 +7,20 @@ import java.util.Scanner;
 
 public class WordCount {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
+		Scanner userInput = new Scanner(System.in);
+		String fileName;
+		
+		System.out.println("What is the file name to be searched?");
+		fileName = userInput.nextLine();
+		File inputFile = new File(fileName); 
+		
+		
+		
+		
+		
+		
 	}
 
 }

a395e17ddcb91f86f0e7498b62045c32d87a0742
Sherlyn Paterson <sherlynpaterson@gmail.com>
2021-01-05 15:46:13 -0500

    updated first exercise from session

--- 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
@@ -2,25 +2,70 @@ package com.techelevator;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class WordCount {
+public class WordCount 
+{
 
 	public static void main(String[] args) 
 	{
+		/*
+		 * 1 - Specify file path (create a File)
+		 * 2 - Create a Scanner to read the FileStream
+		 * 3 - Loop through the stream - scanner.nextLine() or scanner.next()
+		 * 4 - do something with the data on each line
+		 */
 		Scanner userInput = new Scanner(System.in);
-		String fileName;
+		System.out.println("What file do you want to read?");
+		String fileName = userInput.nextLine();
 		
-		System.out.println("What is the file name to be searched?");
-		fileName = userInput.nextLine();
-		File inputFile = new File(fileName); 
+		File file = new File(fileName);
+		// don't even try to read the file if it doesn't exist
+		if(file.exists()) 
+		{
+			readFile(file);
+		}
+		else
+		{
+			System.out.println("The file '" + fileName + "' does not exist.");
+		}
+
+		//knows the text file is in the same directory
 		
+	}
+	
+
+	public static void readFile(File file)
+	{
+		//this is where I will read the file
+		try
+		{
+		Scanner fileScanner = new Scanner(file);
 		
+		String line = "";
+		int wordCount = 0;	
+		int sentenceCount = 0;
 		
-		
-		
-		
+		while(fileScanner.hasNext())
+		{
+			String word = fileScanner.next();
+			System.out.println(word);
+			wordCount++;
+			if(word.endsWith(".") || word.endsWith("!") || word.endsWith("?"))
+			{
+				sentenceCount++;
+			}
+		}
+		System.out.println("Word Count: " + wordCount);
+		System.out.println("Sentence Count: " + sentenceCount);
+		}
+		catch (FileNotFoundException e)
+		{
+			//TODO: handle exception
+		}
+
 	}
 
 }

a43236ba0960e9102b7bb2602d854e6557a5bde2
Katrina Moor <moor.katrina@gmail.com>
2021-01-08 10:31:03 -0500

    capstone update

---
+++ 19_Capstone/java/src/main/java/com/techelevator/ui/UserOutput.java
@@ -0,0 +1,81 @@
+package com.techelevator.ui;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.techelevator.application.ItemInventory;
+import com.techelevator.models.Items;
+
+
+/**
+ * Responsibilities:
+ *      This class should handle formatting and displaying ALL messages to the user
+ * 
+ * Dependencies:
+ *      None
+ */
+
+
+public class UserOutput
+{
+
+	public static void clearScreen() {  
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();  
+	}
+
+	public static void displayMessage(String message)
+	{
+		System.out.println();
+		System.out.println(message);
+		System.out.println();
+	}
+
+	public static void displayHomeScreen()
+	{
+		clearScreen();
+		
+		System.out.println();
+		System.out.println("***************************************************");
+		System.out.println("                      Home");
+		System.out.println("***************************************************");
+		System.out.println();
+	}
+	
+	public static void displayInventory(ItemInventory inventory)
+	{
+		clearScreen();
+
+		System.out.println();
+		System.out.println("***************************************************");
+		System.out.println("                    Items");
+		System.out.println("***************************************************");
+		System.out.println();
+		
+		for(Items items: inventory.getItems())
+		{
+			System.out.println(items.toString());
+		}
+		System.out.println();
+		
+	}
+		List<Items> items = Items.getItems()
+	
+		for(Items : 
+		{
+			
+
+			System.out.println(line);
+		}
+
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.println(String.format("%-61s $ %10.2f", "Cart Total", shoppingCart.getCartTotal()));
+
+		System.out.println();
+	}
+    
+	
+	
+}
